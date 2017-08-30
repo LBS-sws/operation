3d1ed639df139455f80fd113031b2a082244e510
沈超 <shenchao@lbsgroup.com.cn>
2017-08-30 14:25:42 +0800

    刪除倉庫的規格字段，新增單價字段

--- operation/protected/data/opr_warehouse.sql
+++ operation/protected/data/opr_warehouse.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : vm1
-Source Server Version : 50626
-Source Host           : 192.168.1.10:3306
+Source Server         : local
+Source Server Version : 50620
+Source Host           : localhost:3306
 Source Database       : operation
 
 Target Server Type    : MYSQL
-Target Server Version : 50626
+Target Server Version : 50620
 File Encoding         : 65001
 
-Date: 2017-07-10 15:18:59
+Date: 2017-08-30 14:17:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,19 +21,16 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `opr_warehouse`;
 CREATE TABLE `opr_warehouse` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `goods_code` varchar(10) DEFAULT NULL COMMENT '物品編號',
+  `goods_code` varchar(20) DEFAULT NULL COMMENT '物品編號',
+  `classify_id` varchar(11) DEFAULT NULL COMMENT '物品分類的id',
   `name` varchar(30) NOT NULL COMMENT '物品名字',
-  `type` varchar(30) NOT NULL COMMENT '物品規格',
   `unit` varchar(30) NOT NULL COMMENT '物品單位',
-  `inventory` int(10) unsigned DEFAULT '0' COMMENT '庫存',
+  `price` float(30,2) NOT NULL DEFAULT '0.00' COMMENT '單價',
+  `inventory` varchar(10) DEFAULT '0' COMMENT '庫存',
   `city` varchar(30) DEFAULT NULL COMMENT '地區',
   `lcu` varchar(30) CHARACTER SET utf32 DEFAULT NULL,
   `luu` varchar(30) DEFAULT NULL,
   `lcd` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `lud` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='倉庫表';
-
--- ----------------------------
--- Records of opr_warehouse
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='倉庫表';

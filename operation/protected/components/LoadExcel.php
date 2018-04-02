a5c594a2f33051c6fb5d8c9cc9d2c4337faa7eed
沈超 <shenchao@lbsgroup.com.cn>
2018-04-02 17:42:30 +0800

    獲取excel的結果而不是函數

--- operation/protected/components/LoadExcel.php
+++ operation/protected/components/LoadExcel.php
@@ -40,7 +40,8 @@ class LoadExcel {
             /**从第A列开始输出*/
             $arr = array();
             for($currentColumn= 0;$currentColumn<= $allColumn; $currentColumn++){
-                $val = $currentSheet->getCellByColumnAndRow($currentColumn,$currentRow)->getValue();/**ord()将字符转为十进制数*/
+                //$val = $currentSheet->getCellByColumnAndRow($currentColumn,$currentRow)->getValue();/**ord()将字符转为十进制数*/
+                $val = $currentSheet->getCellByColumnAndRow($currentColumn,$currentRow)->getCalculatedValue();//獲取公式后的結果
                 array_push($arr,$val);
             }
             array_push($listBody,$arr);

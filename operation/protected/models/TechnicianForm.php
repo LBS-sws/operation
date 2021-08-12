9e0580118514c370c614697ac5953132369e6cfc
shenchao <shenchao@lbsgroup.com.cn>
2021-08-12 15:10:17 +0800

    外勤領料數量不允許為負數

--- operation/protected/models/TechnicianForm.php
+++ operation/protected/models/TechnicianForm.php
@@ -87,7 +87,7 @@ class TechnicianForm extends CFormModel
                 $message = Yii::t('procurement','goods does not exist');
                 $this->addError($attribute,$message);
                 return false;
-            }else if(!is_numeric($goods["goods_num"])){
+            }else if(!is_numeric($goods["goods_num"])||$goods["goods_num"]<=0){
                 $message = Yii::t('procurement','Goods Number can only be numbered');
                 $this->addError($attribute,$message);
                 return false;

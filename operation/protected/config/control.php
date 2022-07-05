531de08cf92f4c3927c1b378dd7d35c9f6646e4f
LBS IT <percylee@lbsgroup.com.hk>
2022-07-05 10:25:30 +0800

    ID營業報告權限修正 - 2
    YN07才是正確數值

--- operation/protected/config/control.php
+++ operation/protected/config/control.php
@@ -7,5 +7,5 @@ return array(
     'Warehouse Item description'=>array('access'=>'YN04',),//仓库物品说明
 	'Sales Summary Approval - Manager'=>array('access'=>'YN05',),
 	'Technical Overall leaderboard(all city)'=>array('access'=>'YN10',),//综合排行榜（全国)
-	'Report ID Approval (HQ)'=>array('access'=>'YN06',),//權限問題，07還原成06（沈超修改）
+	'Report ID Approval (HQ)'=>array('access'=>'YN07',),//權限問題，07還原成06（沈超修改）, Percy再修改, 07才是正確數值
 );

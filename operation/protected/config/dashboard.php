f53e6ae4716bca6d782d4a50c2a4003c7c8172e8
shenchao <shenchao@lbsgroup.com.cn>
2022-07-05 14:07:42 +0800

    首頁排行榜無法顯示問題

--- operation/protected/config/dashboard.php
+++ operation/protected/config/dashboard.php
@@ -6,9 +6,9 @@ return (array(
     //分类项目排行榜 - 本地
     array('view'=>'rankOtherList', 'row'=>0, 'width'=>'col-md-6', 'access'=>'TL08'),
     //技术部综合排行榜 - 全国
-    array('view'=>'rankAllList', 'row'=>0, 'width'=>'col-md-6', 'access'=>'YN06'),
+    array('view'=>'rankAllList', 'row'=>0, 'width'=>'col-md-6', 'access'=>'YN10'),
     //分类项目排行榜 - 全国
-    array('view'=>'rankOtherAllList', 'row'=>0, 'width'=>'col-md-6', 'access'=>'YN06'),
+    array('view'=>'rankOtherAllList', 'row'=>0, 'width'=>'col-md-6', 'access'=>'YN10'),
 ));
 
 ?>

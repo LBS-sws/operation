794e125c217c3df78cc5c050bc7c34db6454496e
shenchao <shenchao@lbsgroup.com.cn>
2023-04-25 11:34:31 +0800

    U系統多個員工需要平分該訂單的金額

--- operation/protected/models/ServiceMoneyForm.php
+++ operation/protected/models/ServiceMoneyForm.php
@@ -279,7 +279,7 @@ class ServiceMoneyForm extends CFormModel
     public static function getUServiceMoney($year,$month){
         $whereDate = date("Y/m/01",strtotime("{$year}/{$month}/01"));
         $suffix = Yii::app()->params['envSuffix'];
-        $rows = Yii::app()->db->createCommand()->select("a.OT,a.Fee,a.TermCount,a.Staff01,f.ServiceName")
+        $rows = Yii::app()->db->createCommand()->select("a.OT,a.Fee,a.TermCount,a.Staff01,a.Staff02,a.Staff03,f.ServiceName")
             ->from("service{$suffix}.joborder a")
             ->leftJoin("service{$suffix}.service f","a.ServiceType = f.ServiceType")
             ->where("a.Status=3 and date_format(a.JobDate,'%Y/%m/01') = '{$whereDate}'")
@@ -287,26 +287,36 @@ class ServiceMoneyForm extends CFormModel
         $list = array();
         $createList = array("甲醛","隔油池清洗服务","雾化消毒","厨房油烟清洁服务",
             "RA空气净化-延长维保","RA空气净化-随意派","RA空气净化-轻松派");
+        $staffStrList = array("Staff01","Staff02","Staff03");
         if($rows){
             foreach ($rows as $row){
-                if(!key_exists($row["Staff01"],$list)){
-                    $list[$row["Staff01"]]=array(
-                        "code"=>$row["Staff01"],//員工編號
-                        "sum_money"=>0,//服務金額
-                        "night_money"=>0,//夜單金額
-                        "create_money"=>0,//創新金額
-                    );
-                }
                 $row["Fee"] = is_numeric($row["Fee"])?floatval($row["Fee"]):0;
                 $row["TermCount"] = is_numeric($row["TermCount"])?floatval($row["TermCount"]):0;
                 $money = empty($row["TermCount"])?0:$row["Fee"]/$row["TermCount"];
+                $staffCount = 1;
+                $staffCount+= empty($row["Staff02"])?0:1;
+                $staffCount+= empty($row["Staff03"])?0:1;
+                $money = $money/$staffCount;//如果多人，需要平分金額
                 $money = round($money,2);
-                $list[$row["Staff01"]]["sum_money"]+=$money;
-                if(!empty($row["OT"])){
-                    $list[$row["Staff01"]]["night_money"]+=$money;
-                }
-                if(in_array($row["ServiceName"],$createList)){
-                    $list[$row["Staff01"]]["create_money"]+=$money;
+                foreach ($staffStrList as $staffStr){
+                    $staff = $row[$staffStr];
+                    if(!empty($staff)){
+                        if(!key_exists($staff,$list)){
+                            $list[$staff]=array(
+                                "code"=>$staff,//員工編號
+                                "sum_money"=>0,//服務金額
+                                "night_money"=>0,//夜單金額
+                                "create_money"=>0,//創新金額
+                            );
+                        }
+                        $list[$staff]["sum_money"]+=$money;
+                        if(!empty($row["OT"])){
+                            $list[$staff]["night_money"]+=$money;
+                        }
+                        if(in_array($row["ServiceName"],$createList)){
+                            $list[$staff]["create_money"]+=$money;
+                        }
+                    }
                 }
             }
         }

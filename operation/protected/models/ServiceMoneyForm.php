72476deccc09ea9c002b56676375c6366ab3bc0b
shenchao <shenchao@lbsgroup.com.cn>
2024-02-26 17:03:40 +0800

    修改U系统数据来源（还原旧版）

--- operation/protected/models/ServiceMoneyForm.php
+++ operation/protected/models/ServiceMoneyForm.php
@@ -279,9 +279,10 @@ class ServiceMoneyForm extends CFormModel
 
     public static function getUServiceMoney($year,$month){
         //由于2024年1月29日使用了新的U系统，所以使用新代码
+        /*
         $list = SystemU::getTechnicianSNC($year,$month);
         return isset($list["data"])?$list["data"]:array();
-
+*/
         //由于2024年1月29日使用了新的U系统，所以不使用以下代码
         $whereDate = date("Y/m/01",strtotime("{$year}/{$month}/01"));
         $suffix = Yii::app()->params['envSuffix'];
